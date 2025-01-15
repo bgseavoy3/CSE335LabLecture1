c0ce5adbc19ed9d978f2add898a8e7eac2bb111a
bgseavoy3 <89864854+bgseavoy3@users.noreply.github.com>
2025-01-15 16:17:36 -0700

    Commit 1 - Basic Functionality
    Added required greeting functionality, next step is to add the required additional feature as mentioned in the project description

---
+++ swistUIDemo1/swistUIDemo1/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  swistUIDemo1
+//
+//  Created by bseavoy on 1/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var fName: String = ""
+    @State var lName: String = ""
+    @State var greeting: String = ""
+    var body: some View {
+        
+        Text("Welcome to CSE335!")
+        Spacer()
+        
+        HStack {
+            Text("First Name: ")
+            Spacer()
+            Spacer()
+            TextField("please enter your first name here", text: $fName)
+        }
+        HStack {
+            Text("Last Name: ")
+            Spacer()
+            Spacer()
+            TextField("please enter your last name here", text: $lName)
+        }
+        Button("Enter", action: {
+            if(fName != "" && lName != "")
+            {
+                greeting = fName + " " + lName + " Welcome to CSE335"
+            }
+        })
+        Spacer()
+        
+        Text(greeting)
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swistUIDemo1/swistUIDemo1/swistUIDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  swistUIDemo1App.swift
+//  swistUIDemo1
+//
+//  Created by bseavoy on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct swistUIDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
